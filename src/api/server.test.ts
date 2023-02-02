820ee136847d5abdadca2869c43355bc9e232513
Jaroslav Šmolík <grissius@gmail.com>
2023-02-02 16:45:28 +0100

    ✅️ Add API integration test

---
+++ src/api/server.test.ts
@@ -0,0 +1,79 @@
+import assert from 'assert'
+import { server } from './server'
+
+// TODO: Split per feature
+describe('Server (integration)', () => {
+  test('Accessing non-existing project creates a demo list', async () => {
+    const query = `query GetProgress($progressId: ID) {
+            progress(id: $progressId) {
+              displayName
+              id
+              phases {
+                status
+                displayName
+                tasks {
+                  displayName
+                }
+              }
+            }
+          }
+          `
+    const { body } = await server.executeOperation({
+      query,
+      variables: { progressId: '2' },
+    })
+    assert(body.kind === 'single')
+    expect(body.singleResult.data).toMatchInlineSnapshot(`
+{
+  "progress": {
+    "displayName": "My startup progress",
+    "id": "2",
+    "phases": [
+      {
+        "displayName": "Foundation",
+        "status": "OPEN",
+        "tasks": [
+          {
+            "displayName": "Buy domains",
+          },
+          {
+            "displayName": "Select business name",
+          },
+          {
+            "displayName": "Set up virtual office",
+          },
+          {
+            "displayName": "Set mission & vision",
+          },
+        ],
+      },
+      {
+        "displayName": "Discovery",
+        "status": "BLOCKED",
+        "tasks": [
+          {
+            "displayName": "Create roadmap",
+          },
+          {
+            "displayName": "Competitor analyses",
+          },
+        ],
+      },
+      {
+        "displayName": "Delivery",
+        "status": "BLOCKED",
+        "tasks": [
+          {
+            "displayName": "Release marketing website",
+          },
+          {
+            "displayName": "Release MVP",
+          },
+        ],
+      },
+    ],
+  },
+}
+`)
+  })
+})
